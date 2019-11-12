91b3929d63c52f7a1691057cded2b1cd9379646e
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 14:23:36 -0500

    Completed exercise 4, using where to filter query

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,9 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+# Verifying delete
+
+puts "The total number of stores is now: #{Store.count}." 
+
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,29 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+# Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+# Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+Store.create([{name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true}, {name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false}, {name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true}]) do |s|
+end
+
+puts "The total number of stores: #{Store.count}." 
+
+# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_store
+
+@mens_store = Store.where(mens_apparel: true)
+
+
+# @mens_store {|store| puts "Store that carry men's apparely #{store}"}
+
+@mens_store.each do | store |
+  puts "#{store.name} carries men's apparel and has an annual revenue of #{store.annual_revenue}."
+end
+
+@womens_store = Store.where(womens_apparel:true).where("annual_revenue < ?", 1000000)
+
+@womens_store.each do | store |
+  puts "#{store.name} carries women's apparel and has an annual revenue of #{store.annual_revenue}."
+end
+
