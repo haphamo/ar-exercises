1f88f5504b2a62ea0bed2186d546fbe48c8be783
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 15:23:21 -0500

    completed exercise 6, added employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -7,5 +7,10 @@ require_relative './exercise_5'
 
 puts "Exercise 6"
 puts "----------"
+# @store1 = Store.find_by(id:1)
 
-# Your code goes here ...
+@store1.employees.create([{first_name: "Bubblegum", last_name: "Princess", hourly_rate: 60}, {first_name: "Finn", last_name: "Human", hourly_rate: 65}, {first_name: "Jake", last_name: "Dog", hourly_rate: 70}])
+
+
+@store2.employees.create([{first_name: "Marcelline", last_name: "Vampire Queen", hourly_rate: 60}, {first_name: "Lumpy Space", last_name: "Princess", hourly_rate: 60}, {first_name: "Flame", last_name: "Princess", hourly_rate: 80}])
+
