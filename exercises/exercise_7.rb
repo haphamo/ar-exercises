bd490e7afbe17e087b608fd898f0d49bdd5bfd11
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 17:34:47 -0500

    completed final exercise, validations and error

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,22 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Validations
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+
+
+puts "Please enter a store name."
+@input = gets.chomp
+
+
+new_store = Store.create(name: @input)
+
+# puts new_store.valid?
+puts "Error": new_store.errors.messages
+
+
+
