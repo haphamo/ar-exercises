26fe4f598e074a6bbd4fcb0b67bcf1f2bb03a84b
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 14:49:43 -0500

    Completed exercise 5, used sum and count calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,16 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+@number_of_stores = Store.count
+
+puts "The total revenue is $#{@total_revenue}."
+
+@average = @total_revenue/@number_of_stores
+
+puts "The average annual revenue is $#{@average}."
+# Find stores that generate =< 1M, chain where and size(or count)
+
+@results = Store.where("annual_revenue >= ?", 1000000).count
+
+puts "Number of stores that make 1M or more: #{@results}."
