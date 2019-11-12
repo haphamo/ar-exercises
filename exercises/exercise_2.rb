559a71ddbe5eb20103d0f614b84baf420253e8c1
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 13:41:17 -0500

    Completed exercise 2, updated data in the db

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,10 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+@store1 = Store.find_by(id:1)
+@store2 = Store.find_by(id:2)
+puts "The first store is #{@store1.name} and second is #{@store2.name}."
+# Updated store1 name
+@store1.update(name: "Vaughan Mills")
+puts "Store1 new name is: #{@store1.name}"
 
-# Your code goes here ...
