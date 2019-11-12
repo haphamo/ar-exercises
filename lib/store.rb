4649bdec0671da4041dcdbc2925bf4775e753429
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-12 17:38:12 -0500

    Forgot to commit the validation changes on model files

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, length: { minimum: 3}, presence: true
+  validates :annual_revenue, numericality: { only_integer:true , greater_than: 0 }
 end
+
+
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+
+# validates :custom
+
+# def custom
+#   if :mens_apparel.valid? || :womens_apparel.valid?
+# end
